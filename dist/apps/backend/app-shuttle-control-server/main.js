fe0e9ac758c280fef87185e9be8b1b56782ade13
goju2k <goju2k@naver.com>
2024-02-02 12:25:15 +0900

    server added

---
+++ dist/apps/backend/app-shuttle-control-server/main.js
@@ -0,0 +1,70 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((module) => {
+
+module.exports = require("tslib");
+
+/***/ }),
+/* 2 */
+/***/ ((module) => {
+
+module.exports = require("express");
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+var _a;
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const tslib_1 = __webpack_require__(1);
+const express_1 = tslib_1.__importDefault(__webpack_require__(2));
+const host = (_a = process.env.HOST) !== null && _a !== void 0 ? _a : 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    console.log('hello');
+    res.send({ message: 'Hello API' });
+});
+app.listen(port, host, () => {
+    console.log(`[ ready ] http://${host}:${port}`);
+});
+
+})();
+
+var __webpack_export_target__ = exports;
+for(var i in __webpack_exports__) __webpack_export_target__[i] = __webpack_exports__[i];
+if(__webpack_exports__.__esModule) Object.defineProperty(__webpack_export_target__, "__esModule", { value: true });
+/******/ })()
+;
