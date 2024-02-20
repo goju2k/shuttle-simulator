f4b84e188de315e004ca33ec063691ec6fe1d095
goju2k <goju2k@naver.com>
2024-02-20 23:14:16 +0900

    Revert "server removed"
    This reverts commit 8a5df08e54de6526374b5f0fdc554c93de962081.

---
+++ apps/backend/app-shuttle-control-server/jest.config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+export default {
+  displayName: 'app-shuttle-control-server',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../../coverage/apps/backend/app-shuttle-control-server',
+};
