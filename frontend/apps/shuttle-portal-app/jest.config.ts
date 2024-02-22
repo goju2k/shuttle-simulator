a0f659936fc9455d9612274ebdbd5f9d987b2830
goju2k <goju2k@naver.com>
2024-02-22 15:46:49 +0900

    sample next app added

---
+++ frontend/apps/shuttle-portal-app/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'shuttle-portal-app',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/frontend/apps/shuttle-portal-app',
+};
---
+++ frontend/apps/shuttle-portal-app/src/app/privacy-policy/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  
+`;
+
+export default function PrivacyPolicyPage() {
+
+  return (
+    <StyledPage>
+      개인정보 처리방침입니다.
+    </StyledPage>
+  );
+
+}
